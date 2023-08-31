77dbcc6b86d55d8c1c1da51cd9369e6a0acc864d
Carlos Felipe <carlos@carlosfelipe.dev>
2023-08-31 11:31:43 -0300

    Start lesson UI improvements

--- lib/ui/pages/flashcards/flashcards.page.dart
+++ lib/ui/pages/flashcards/flashcards.page.dart
@@ -4,6 +4,7 @@ import 'package:flashcards/ui/pages/flashcard-search/flashcard_search.dart';
 import 'package:flashcards/ui/pages/lesson/lesson.page.arguments.dart';
 import 'package:flashcards/ui/widgets/bottom_sheet_dialog.dart';
 import 'package:flashcards/ui/widgets/flashcards_grid.dart';
+import 'package:flashcards/ui/widgets/handle.dart';
 import 'package:flashcards/ui/widgets/lesson_generator_form.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
@@ -16,9 +17,7 @@ class FlashcardsPage extends StatefulWidget {
 }
 
 class _FlashcardsPageState extends State<FlashcardsPage> {
-
   GlobalKey<FlashcardsGridState> _flashcardsGridKey = GlobalKey();
-
 
   bool shouldHideFloatingActionButton = false;
 
@@ -26,13 +25,16 @@ class _FlashcardsPageState extends State<FlashcardsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: FlashcardsGrid(
-          key: _flashcardsGridKey,
-          onScrollBottomEnter: () => setState(() { shouldHideFloatingActionButton = true; }),
-          onScrollBottomExit: () => setState(() { shouldHideFloatingActionButton = false; }),
-          numberOfRowsPerCategory: 2,
-        )
-      ),
+          child: FlashcardsGrid(
+        key: _flashcardsGridKey,
+        onScrollBottomEnter: () => setState(() {
+          shouldHideFloatingActionButton = true;
+        }),
+        onScrollBottomExit: () => setState(() {
+          shouldHideFloatingActionButton = false;
+        }),
+        numberOfRowsPerCategory: 2,
+      )),
       floatingActionButton: SpeedDial(
         animatedIcon: AnimatedIcons.menu_close,
         animatedIconTheme: IconThemeData(size: 22.0),
@@ -42,117 +44,126 @@ class _FlashcardsPageState extends State<FlashcardsPage> {
         overlayColor: Colors.black,
         children: [
           SpeedDialChild(
-            elevation: 2,
-            child: Icon(Icons.add),
-            onTap: () async {
-              await Navigator.of(context).pushNamed(RoutesPaths.flashcardEditor);
-              _flashcardsGridKey.currentState?.fetchFlashcards();
-            },
-            backgroundColor: Theme.of(context).primaryColor,
-            foregroundColor: Theme.of(context).colorScheme.onPrimary,
-            labelWidget: Container(
-              margin: EdgeInsets.only(right: 12),
-              child: Text(
-                MyAppLocalizations.of(context).createFlashcard.toUpperCase(),
-                style: speedDialChildTextStyle
-              ),
-            )
-          ),
+              elevation: 2,
+              child: Icon(Icons.add),
+              onTap: () async {
+                await Navigator.of(context)
+                    .pushNamed(RoutesPaths.flashcardEditor);
+                _flashcardsGridKey.currentState?.fetchFlashcards();
+              },
+              backgroundColor: Theme.of(context).primaryColor,
+              foregroundColor: Theme.of(context).colorScheme.onPrimary,
+              labelWidget: Container(
+                margin: EdgeInsets.only(right: 12),
+                child: Text(
+                    MyAppLocalizations.of(context)
+                        .createFlashcard
+                        .toUpperCase(),
+                    style: speedDialChildTextStyle),
+              )),
           SpeedDialChild(
-            elevation: 2,
-            child: Icon(Icons.play_arrow),
-            onTap: () => showLessonGeneratorForm(context),
-            backgroundColor: Theme.of(context).primaryColor,
-            foregroundColor: Theme.of(context).colorScheme.onPrimary,
-            labelWidget: Container(
-              margin: EdgeInsets.only(right: 12),
-              child: Text(
-                MyAppLocalizations.of(context).practice.toUpperCase(),
-                style: speedDialChildTextStyle
-              ),
-            )
-          ),
+              elevation: 2,
+              child: Icon(Icons.play_arrow),
+              onTap: () => showLessonGeneratorForm(context),
+              backgroundColor: Theme.of(context).primaryColor,
+              foregroundColor: Theme.of(context).colorScheme.onPrimary,
+              labelWidget: Container(
+                margin: EdgeInsets.only(right: 12),
+                child: Text(
+                    MyAppLocalizations.of(context).practice.toUpperCase(),
+                    style: speedDialChildTextStyle),
+              )),
           SpeedDialChild(
-            elevation: 2,
-            child: Icon(Icons.label),
-            onTap: () async {
-              await Navigator.of(context).pushNamed(RoutesPaths.categoryManager);
-              _flashcardsGridKey.currentState?.fetchFlashcards();
-            },
-            backgroundColor: Theme.of(context).primaryColor,
-            foregroundColor: Theme.of(context).colorScheme.onPrimary,
-            labelWidget: Container(
-              margin: EdgeInsets.only(right: 12),
-              child: Text(
-                MyAppLocalizations.of(context).manageCategories.toUpperCase(),
-                style: speedDialChildTextStyle
-              ),
-            )
-          ),
+              elevation: 2,
+              child: Icon(Icons.label),
+              onTap: () async {
+                await Navigator.of(context)
+                    .pushNamed(RoutesPaths.categoryManager);
+                _flashcardsGridKey.currentState?.fetchFlashcards();
+              },
+              backgroundColor: Theme.of(context).primaryColor,
+              foregroundColor: Theme.of(context).colorScheme.onPrimary,
+              labelWidget: Container(
+                margin: EdgeInsets.only(right: 12),
+                child: Text(
+                    MyAppLocalizations.of(context)
+                        .manageCategories
+                        .toUpperCase(),
+                    style: speedDialChildTextStyle),
+              )),
           SpeedDialChild(
-            elevation: 2,
-            child: Icon(Icons.search),
-            onTap: () async {
-              await showSearch(context: context, delegate: FlashcardSearch());
-              _flashcardsGridKey.currentState?.fetchFlashcards();
-            },
-            backgroundColor: Theme.of(context).primaryColor,
-            foregroundColor: Theme.of(context).colorScheme.onPrimary,
-            labelWidget: Container(
-              margin: EdgeInsets.only(right: 12),
-              child: Text(
-                MyAppLocalizations.of(context).searchFlashcard.toUpperCase(),
-                style: speedDialChildTextStyle
-              ),
-            )
-          ),
+              elevation: 2,
+              child: Icon(Icons.search),
+              onTap: () async {
+                await showSearch(context: context, delegate: FlashcardSearch());
+                _flashcardsGridKey.currentState?.fetchFlashcards();
+              },
+              backgroundColor: Theme.of(context).primaryColor,
+              foregroundColor: Theme.of(context).colorScheme.onPrimary,
+              labelWidget: Container(
+                margin: EdgeInsets.only(right: 12),
+                child: Text(
+                    MyAppLocalizations.of(context)
+                        .searchFlashcard
+                        .toUpperCase(),
+                    style: speedDialChildTextStyle),
+              )),
           SpeedDialChild(
-            elevation: 2,
-            child: Icon(Icons.settings),
-            onTap: () => Navigator.of(context).pushNamed(RoutesPaths.settings),
-            backgroundColor: Theme.of(context).primaryColor,
-            foregroundColor: Theme.of(context).colorScheme.onPrimary,
-            labelWidget: Container(
-              margin: EdgeInsets.only(right: 12),
-              child: Text(
-                MyAppLocalizations.of(context).settings.toUpperCase(),
-                style: speedDialChildTextStyle
-              ),
-            )
-          ),
+              elevation: 2,
+              child: Icon(Icons.settings),
+              onTap: () =>
+                  Navigator.of(context).pushNamed(RoutesPaths.settings),
+              backgroundColor: Theme.of(context).primaryColor,
+              foregroundColor: Theme.of(context).colorScheme.onPrimary,
+              labelWidget: Container(
+                margin: EdgeInsets.only(right: 12),
+                child: Text(
+                    MyAppLocalizations.of(context).settings.toUpperCase(),
+                    style: speedDialChildTextStyle),
+              )),
         ],
       ),
     );
   }
 
-  TextStyle get speedDialChildTextStyle => TextStyle(fontWeight: FontWeight.bold, color: Colors.white);
+  TextStyle get speedDialChildTextStyle =>
+      TextStyle(fontWeight: FontWeight.bold, color: Colors.white);
 
   void showLessonGeneratorForm(BuildContext context) async {
     final height = MediaQuery.of(context).size.height;
     final padding = MediaQuery.of(context).padding;
     final safeHeight = height - padding.top - padding.bottom;
     await showModalBottomSheet(
-      context: context,
-      isScrollControlled: true,
-      constraints: BoxConstraints(
-        maxHeight: safeHeight,
-        minHeight: MediaQuery.of(context).size.height / 2
-      ),
-      builder: (context) {
-        return BottomSheetDialog(
-          padding: EdgeInsets.all(12.0),
-          child: LessonGeneratorForm(
-            onGenerate: (flashcards) async {
-              if (ModalRoute.of(context)!.isCurrent) {
-                Navigator.of(context).pop();
-                await Navigator.of(context).pushNamed(RoutesPaths.lesson, arguments: LessonPageArguments(flashcards));
-                _flashcardsGridKey.currentState?.fetchFlashcards();
-              }
-            },
-          ),
-        );
-      }
-    );
+        context: context,
+        isScrollControlled: true,
+        constraints: BoxConstraints(maxHeight: safeHeight),
+        builder: (context) {
+          return BottomSheetDialog(
+            padding: EdgeInsets.all(12.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Handle(),
+                SizedBox(
+                  height: 8.0,
+                ),
+                Flexible(
+                  child: LessonGeneratorForm(
+                    onGenerate: (flashcards) async {
+                      if (ModalRoute.of(context)!.isCurrent) {
+                        Navigator.of(context).pop();
+                        await Navigator.of(context).pushNamed(
+                            RoutesPaths.lesson,
+                            arguments: LessonPageArguments(flashcards));
+                        _flashcardsGridKey.currentState?.fetchFlashcards();
+                      }
+                    },
+                  ),
+                ),
+              ],
+            ),
+          );
+        });
   }
 
   @override
--- lib/ui/widgets/lesson_generator_form.dart
+++ lib/ui/widgets/lesson_generator_form.dart
@@ -11,18 +11,23 @@ import 'package:get_it/get_it.dart';
 class LessonGeneratorForm extends StatefulWidget {
   final void Function(List<Flashcard>) onGenerate;
 
-  const LessonGeneratorForm({Key? key, required this.onGenerate}) : super(key: key);
-
+  const LessonGeneratorForm({Key? key, required this.onGenerate})
+      : super(key: key);
 
   @override
   _LessonGeneratorFormState createState() => _LessonGeneratorFormState();
 }
 
-enum _LessonGeneratorSteps { selectCategory, selectQuantity, generatingLesson, generateLessonFailed }
+enum _LessonGeneratorSteps {
+  selectCategory,
+  selectQuantity,
+  generatingLesson,
+  generateLessonFailed
+}
 
 class _LessonGeneratorFormState extends State<LessonGeneratorForm> {
-
-  final FindCategoriesCountingFlashcardsUseCase findCategoriesCountingFlashcards = GetIt.I();
+  final FindCategoriesCountingFlashcardsUseCase
+      findCategoriesCountingFlashcards = GetIt.I();
   final GenerateLessonUseCase generateLessonUseCase = GetIt.I();
   late Future<List<CategoryFlashcardsCount>> categoriesFlashcards;
   _LessonGeneratorSteps step = _LessonGeneratorSteps.selectCategory;
@@ -39,51 +44,57 @@ class _LessonGeneratorFormState extends State<LessonGeneratorForm> {
   @override
   Widget build(BuildContext context) {
     return Form(
-      child: FutureBuilder<List<CategoryFlashcardsCount>>(
-        future: categoriesFlashcards,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) return pending;
-          if (snapshot.hasError) return tryAgainFetchFlashcards;
-          if (snapshot.requireData.isEmpty) return noFlashcardsWarningText;
-          if (snapshot.requireData.map((e) => e.flashcardsCount).reduce((a, b) => a + b) == 0)
-            return noFlashcardsWarningText;
-          if (step == _LessonGeneratorSteps.selectCategory) return buildSelectCategoryStep(snapshot.requireData);
-          if (step == _LessonGeneratorSteps.selectQuantity) return selectQuantityStep;
-          if (step == _LessonGeneratorSteps.generatingLesson) return pending;
-          return tryAgainGenerateLesson;
-        }
-      )
-    );
+        child: FutureBuilder<List<CategoryFlashcardsCount>>(
+            future: categoriesFlashcards,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting)
+                return pending;
+              if (snapshot.hasError) return tryAgainFetchFlashcards;
+              if (snapshot.requireData.isEmpty) return noFlashcardsWarningText;
+              if (snapshot.requireData
+                      .map((e) => e.flashcardsCount)
+                      .reduce((a, b) => a + b) ==
+                  0) return noFlashcardsWarningText;
+              if (step == _LessonGeneratorSteps.selectCategory)
+                return buildSelectCategoryStep(snapshot.requireData);
+              if (step == _LessonGeneratorSteps.selectQuantity)
+                return selectQuantityStep;
+              if (step == _LessonGeneratorSteps.generatingLesson)
+                return pending;
+              return tryAgainGenerateLesson;
+            }));
   }
 
   Widget get pending {
-    return Center(child: CircularProgressIndicator(),);
+    return Center(
+      child: CircularProgressIndicator(),
+    );
   }
 
   Widget get tryAgainFetchFlashcards {
     return Center(
       child: TryAgain(
-        message: MyAppLocalizations.of(context).defaultErrorMessage,
-        onPressed: () {
-          setState(() {
-            categoriesFlashcards = findCategoriesCountingFlashcards();
-          });
-        }
-      ),
+          message: MyAppLocalizations.of(context).defaultErrorMessage,
+          onPressed: () {
+            setState(() {
+              categoriesFlashcards = findCategoriesCountingFlashcards();
+            });
+          }),
     );
   }
 
   Widget get noFlashcardsWarningText {
     return Center(
-      child: Text(
-        MyAppLocalizations.of(context).practiceNeedsFlashcardsMessage,
-        textAlign: TextAlign.center,
-      )
-    );
+        child: Text(
+      MyAppLocalizations.of(context).practiceNeedsFlashcardsMessage,
+      textAlign: TextAlign.center,
+    ));
   }
 
-  Widget buildSelectCategoryStep(List<CategoryFlashcardsCount> categoriesCoutingFlashcards) {
-    categoriesCoutingFlashcards.retainWhere((element) => element.flashcardsCount > 0);
+  Widget buildSelectCategoryStep(
+      List<CategoryFlashcardsCount> categoriesCoutingFlashcards) {
+    categoriesCoutingFlashcards
+        .retainWhere((element) => element.flashcardsCount > 0);
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
@@ -91,40 +102,47 @@ class _LessonGeneratorFormState extends State<LessonGeneratorForm> {
           padding: const EdgeInsets.all(8.0),
           child: Text(
             MyAppLocalizations.of(context).selectCategory,
-            style: Theme.of(context).textTheme.subtitle1!.copyWith(fontWeight: FontWeight.bold),
+            style: Theme.of(context)
+                .textTheme
+                .subtitle1!
+                .copyWith(fontWeight: FontWeight.bold),
           ),
         ),
         Flexible(
-          child: ListView.builder(
-            itemCount: categoriesCoutingFlashcards.length,
-            itemBuilder: (context, index) {
-              final item = categoriesCoutingFlashcards.elementAt(index);
-              return ListTile(
-                title: Text(
-                  item.category?.name ?? MyAppLocalizations.of(context).uncategorized,
-                  style: Theme.of(context).textTheme.subtitle2
-                ),
-                trailing: chosenCategory != null && chosenCategory!.category == item.category ? Icon(Icons.check) : null,
-                onTap: () {
-                  setState(() {
-                    chosenCategory = item;
-                  });
-                },
-              );
-            }
-          )
-        ),
+            child: ListView.builder(
+                itemCount: categoriesCoutingFlashcards.length,
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  final item = categoriesCoutingFlashcards.elementAt(index);
+                  return ListTile(
+                    title: Text(
+                        item.category?.name ??
+                            MyAppLocalizations.of(context).uncategorized,
+                        style: Theme.of(context).textTheme.subtitle2),
+                    trailing: chosenCategory != null &&
+                            chosenCategory!.category == item.category
+                        ? Icon(Icons.check)
+                        : null,
+                    onTap: () {
+                      setState(() {
+                        chosenCategory = item;
+                      });
+                    },
+                  );
+                })),
         SizedBox(height: 8),
         Container(
           width: double.infinity,
           child: ElevatedButton(
-            onPressed: chosenCategory != null ? () {
-              setState(() {
-                step = _LessonGeneratorSteps.selectQuantity;
-              });
-            } : null,
-            child: Text(MyAppLocalizations.of(context).nextStep.toUpperCase())
-          ),
+              onPressed: chosenCategory != null
+                  ? () {
+                      setState(() {
+                        step = _LessonGeneratorSteps.selectQuantity;
+                      });
+                    }
+                  : null,
+              child:
+                  Text(MyAppLocalizations.of(context).nextStep.toUpperCase())),
         )
       ],
     );
@@ -138,31 +156,33 @@ class _LessonGeneratorFormState extends State<LessonGeneratorForm> {
           keyboardType: TextInputType.number,
           autofocus: true,
           decoration: InputDecoration(
-            labelText: MyAppLocalizations.of(context).flashcardsQuantity,
-            counterText: MyAppLocalizations.of(context).typeNumberBetween(1, chosenCategory!.flashcardsCount)
-          ),
+              labelText: MyAppLocalizations.of(context).flashcardsQuantity,
+              counterText: MyAppLocalizations.of(context)
+                  .typeNumberBetween(1, chosenCategory!.flashcardsCount)),
           onChanged: (value) {
             setState(() {
               try {
                 count = int.parse(value);
               } catch (_) {
-                count = 0;  
+                count = 0;
               }
             });
           },
         ),
-        SizedBox(height: 8,),
+        SizedBox(
+          height: 8,
+        ),
         Container(
           width: double.infinity,
           child: ElevatedButton(
-            onPressed: count > 0 && count <= chosenCategory!.flashcardsCount ? () {
-              generateLesson(LessonSettings(
-                category: chosenCategory!.category,
-                flashcardsCount: count
-              ));
-            } : null,
-            child: Text(MyAppLocalizations.of(context).start.toUpperCase())
-          ),
+              onPressed: count > 0 && count <= chosenCategory!.flashcardsCount
+                  ? () {
+                      generateLesson(LessonSettings(
+                          category: chosenCategory!.category,
+                          flashcardsCount: count));
+                    }
+                  : null,
+              child: Text(MyAppLocalizations.of(context).start.toUpperCase())),
         )
       ],
     );
@@ -170,14 +190,11 @@ class _LessonGeneratorFormState extends State<LessonGeneratorForm> {
 
   Widget get tryAgainGenerateLesson {
     return TryAgain(
-      message: MyAppLocalizations.of(context).lessonGenerationErrorMessage,
-      onPressed: () {
-        generateLesson(LessonSettings(
-          category: chosenCategory!.category,
-          flashcardsCount: count
-        ));
-      }
-    );
+        message: MyAppLocalizations.of(context).lessonGenerationErrorMessage,
+        onPressed: () {
+          generateLesson(LessonSettings(
+              category: chosenCategory!.category, flashcardsCount: count));
+        });
   }
 
   void generateLesson(LessonSettings settings) async {
